45e628fb6109c2fcbd685dc12f4fa2bf72739464
GeorgeM101 <georgmboya@gmail.com>
2022-03-31 04:54:28 +0300

    created location model

--- flags/models.py
+++ flags/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserLocation(models.model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telephone = models.CharField(max_length=20, null=True, blank=True)
+    longitude = models.CharField(verbose_name='Longitude', max_length=50, null=True, blank=True)
+    latitude = models.CharField(verbose_name='Latitude', max_length=50, null=True, blank=True)
